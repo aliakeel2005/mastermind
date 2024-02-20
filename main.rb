166dc74cd6cf9fc3f6913a06063f379b0222d61c
aliakeel2005 <ali.akeelqq123@gmail.com>
2024-02-20 14:55:28 +0300

    computer now generates random colors

--- main.rb
+++ main.rb
@@ -1,13 +1,26 @@
 # ASCII representaion of board
-# |eeee| |EEEE|
-# |eeee| |EEEE|
+# make computer generate code
+# make random number generator from 1 to 6
+# make object with numbers corrosponding to their colors
+# OBJ = {1: red, 2: blue, 3: yellow}
+# 
+
+module RandomColors
+  def generate_color
+    colors = {1 => 'red', 2 => 'blue', 3 => 'yellow', 4 => 'green', 5 => 'brown', 6 => 'purple'}
+    colors[rand(1..6)]
+  end
+end
+
 class Board
+  include RandomColors
+  attr_accessor :code
   def initialize
-    
+    @code = {1 => generate_color, 2 => generate_color, 3 => generate_color, 4 => generate_color}
   end
 
   def display_board
-    puts "|eeee| |EEEE| \n" + 
+    puts "|#{@code[1]}eee| |EEEE| \n" + 
          "|eeee| |EEEE| \n" +
          "|eeee| |EEEE| \n" +
          "|eeee| |EEEE| \n" +
@@ -22,5 +35,6 @@ class Board
   end
 end
 
-board = Board.new
+board = Board.new 
+  # p color = {1 => board.generate_color, 2 => board.generate_color, 3 => board.generate_color, 4 => board.generate_color}
 board.display_board
