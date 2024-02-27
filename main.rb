1f836d45c4af6eeda832d8269d7c2ca1c7cc7373
aliakeel2005 <ali.akeelqq123@gmail.com>
2024-02-27 11:15:18 +0300

    player can now be the code_maker

--- main.rb
+++ main.rb
@@ -1,18 +1,3 @@
-# ASCII representaion of board
-# make computer generate code
-# make random number generator from 1 to 6
-# make hash with numbers corrosponding to their colors
-# loop ask player for code until:
-# code is correct or turn 12
-# issue: @code of cpu has a color in each item, player_guesses has entire guess in each item
-# split answer of player and put in player_guess array
-# compare the array to @code, if matching, end game
-# also put that guess in the player_guesses array
-# use player_guesses array to display on board later
-# issue: display_board too big, make it display one row per turn
-# DsplayBoard is now a module
-# simply adds the player string into the string and displays it
-# if player
 module RandomColors
   def generate_color
     colors = %w[red blue yellow green brown purple]
@@ -40,32 +25,62 @@ module Pegs
   end
 end
 
-class Player
+module CodeMaker
+  include RandomColors
   include DisplayBoard
-  include Pegs
-  def game_begin
-    (1..12).each do |a|
-      string = gets.chomp # "yellow, red, blue"
-      player_guess = string.split # ["yellow", "red", "blue"] [purple, blue, green]
-      add_pegs(string, player_guess)
-      break if player_guess == @code
+  def code_maker
+    pegs = []
+    @player_code = gets.chomp
+    past_guess = []
+    (1..12).each do
+      computer_guess = [generate_color, generate_color, generate_color, generate_color]
+      if !pegs.empty? 
+        past_guess.shuffle!
+        (0..pegs.length).each do |a|
+          computer_guess[a] = past_guess[a] 
+        end
+      end
+      past_guess = computer_guess
+      display_board(computer_guess, pegs)
+      if computer_guess == @player_code
+        break
+      else
+        puts 'any hints?'
+        pegs = []
+        pegs << gets.chomp
+      end
     end
   end
 end
 
-class Board < Player
+
+class CodeBreaker
+  include DisplayBoard
+  include Pegs
+  def game_begin
+    (1..12).each do
+      string = gets.chomp
+      player_guess = string.split
+      add_pegs(string, player_guess)
+      break if player_guess == @code
+    end
+    puts "the code was #{@code}"
+  end
+end
+
+class Board < CodeBreaker
   include RandomColors
-  attr_accessor :code
+  include CodeMaker
 
   def initialize
     super
     @code = [generate_color, generate_color, generate_color, generate_color]
   end
-
-  def show_code
-    puts @code # debug method
-  end
 end
 board = Board.new
-board.show_code
-board.game_begin
+puts '1- code breaker 2- code maker'
+if gets.chomp == '1'
+  board.game_begin
+else
+  board.code_maker
+end
